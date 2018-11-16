41bac20d7b58b6f0e4ebae3acedfac2dcc9b4570
Administrator <442600295@qq.com>
2018-11-16 09:45:53 +0800

    init

--- src/main/java/com/huruwo/demo/Application.java
+++ src/main/java/com/huruwo/demo/Application.java
@@ -6,5 +6,6 @@ import com.huruwo.demo.middleware.AuthMiddleware;
 public class Application {
     public static void main(String[] args) {
         Blade.of().listen(880).use(new AuthMiddleware()).start(Application.class, args);
+
     }
 }
